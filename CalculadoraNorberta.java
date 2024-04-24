9330cb6adf2673f638f101b2b49a3cbcf0a8f707
Norberta Mendoza Sandoval <norymensan@gmail.com>
2024-04-24 17:44:51 -0600

    feature/norberta-mendoza-g20-mujer-digital

---
+++ CalculadoraNorberta.java
@@ -0,0 +1,25 @@
+public class CalculadoraNorberta {
+    public static void main(String[] args) {
+        //las variables de valor que calcularemos
+
+        int valor1 = 20;
+        int valor2 = 30;
+
+        //Suma
+
+        int suma = valor1 + valor2;
+        System.out.println("El valor de mi suma es " +suma);
+
+        //Resta
+        int resta = valor1 - valor2;
+        System.out.println("El valor de mi resta es " +resta);
+
+        //Multiplicación
+        int multiplicación = valor1 * valor2;
+        System.out.println("El valor de mi multiplicación es " +multiplicación);
+
+        //División
+        int división = valor1 / valor2;
+        System.out.println("El valor de mi división es " +división);
+    }
+}
