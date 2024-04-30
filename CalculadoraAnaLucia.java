4c3da42c8d173f156a968f191441a8c5b3e543d8
LUMART21 <163286784+LUMART21@users.noreply.github.com>
2024-04-30 09:09:41 -0600

    Add files via upload

---
+++ CalculadoraAnaLucia.java
@@ -0,0 +1,28 @@
+package practicas.calculadora;
+
+public class CalculadoraAnaLucia {
+    public static void main(String args[]){
+        // Las variables de valo que calcularemos
+
+        int valor1 = 20;
+        int valor2 = 30;
+
+        // Suma
+        int suma = valor1+ valor2;
+        System.out.println("el valor de mi suma es " +suma);
+
+        //Resta
+        int resta = valor1- valor2;
+        System.out.println("el valor de mi resta es " -resta);
+
+        //Multiplicacion
+int multiplicacion = valor1* valor2;
+System.out.println("el valor de mi multiplicacion es " *multiplicacion);
+
+        //Division
+        int Division = valor1/ valor2;
+        System.out.println("el valor de mi division es " /division);
+
+    }
+}
+
