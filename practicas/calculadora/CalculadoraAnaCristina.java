a1a23bead1d7359527106e498a1a0a619b5a1330
Ana Cristina Valenzuela Villalobos <cristina.valenvi@gmail.com>
2024-05-06 21:30:19 -0600

    Feature/Ana C Valenzuela calculadora

---
+++ practicas/calculadora/CalculadoraAnaCristina.java
@@ -0,0 +1,28 @@
+
+
+public class CalculadoraAnaCristina {
+    public static void main(String args[]){
+        // Las variables del valor que calcularemos
+
+        int num1 = 20;
+        int num2 = 30;
+        int suma;
+        int resta;
+        int multiplicacion;
+        int division;
+
+
+
+        suma = num1 + num2;
+        resta = num1 - num2;
+        multiplicacion = num1 * num2;
+        division = num1 / num2;
+
+
+        System.out.println("El resultado de la suma es: " + suma);
+        System.out.println("El resultado de la resta es: " + resta);
+        System.out.println("El resultado de la multiplicacion es: " + multiplicacion);
+        System.out.println("El resultado de la division es: " + division);
+
+    }
+}
