84ec0fa87b2d24f429bc3e096139fd8bbc4a95fc
Alberto Luebbert <>
2024-05-07 18:02:03 -0600

    :sparkles: Clase Tipo de datos - 7 Mayo

---
+++ Tipos.java
@@ -0,0 +1,57 @@
+public class Tipos {
+    public static void main(String[] args) {
+        // tipos de datos
+        // Primitivos
+        // int, float, double, boolean, char
+
+        // Int - Valores enteros
+        // Menos infinito a infinito
+        // que no tiene valor decimal dentro de su esturctura.
+        int edad = 30;
+
+        // float
+        // 32 bits
+        // Menos infinito a Infinito
+        // tiene un valor compuesto de un número entero y numeros decimales.
+        float pi = 3.1416f;
+
+        // double
+        // 64 bits
+        // menos infinito a infinito
+        // tiene un valor compuesto por al menos un número entero y n números decimales.
+        double valorPi = 3.141592653;
+
+        // Boolean
+        // Es una variable que tendrá
+        // inicamente 2 valores
+        // true o false
+        // verdadero o falso
+        // Calculo de los Operadores Lógicos (And, OR, Not)
+        boolean abrimosTercerOjo = true;
+        boolean hoyEsUnDiaFresco = false;
+
+        // Char
+        // Es una variable con un valor de 0 a 65535
+        // ideal para letras
+        char letra1 = 'a';
+        char letra2 = 'A';
+
+        // Tipo de datos - Serie
+        // String
+        // Puede almacenar en su interior
+        // Letraas, simbolos y números
+        String nombre = "Guadalupe Ines";
+        String email = "alberto.luebbert@jamexico.org.mx";
+        String holaMundo = "¡Hola Mundo!";
+
+        // Ejemplos
+        // Representar el clima del día de hoy
+        double climaHoy = 31;
+
+        // Representar el RFC
+        String rfccaro = "MALC001029AM7";
+        String curp = "MESN010228MJCNNRA8";
+        String datosWendy = "CEMW940411AM6";
+
+    }
+}
